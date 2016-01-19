77721c362c1da29a98394214a6a6138ab14b7c8d
Larry_SU <larrysu1115@gmail.com>
2016-01-19 17:40:28 +0800

    create mesos cluster with 3 masters, and N slaves.

---
+++ gce-mesos-cluster/create-mesos-cluster.sh
@@ -0,0 +1,3 @@
+./utils.sh -c create-master -n mesos-master-1,mesos-master-2,mesos-master-3
+
+./utils.sh -c create-slave -n mesos-slave-1,mesos-slave-2,mesos-slave-3,mesos-slave-4
---
+++ gce-mesos-cluster/delete-mesos-cluster.sh
@@ -0,0 +1,3 @@
+./utils.sh -c delete-vms -n mesos-master-1,mesos-master-2,mesos-master-3
+
+./utils.sh -c delete-vms -n mesos-slave-1,mesos-slave-2,mesos-slave-3,mesos-slave-4
